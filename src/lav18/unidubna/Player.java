561c5066ab5123fb6aa6d8ef80e2f97c5c3cb5b2
Bnnrymndy@gmail.com <Bnnrymndy@gmail.com>
2021-09-02 23:39:54 +0300

    Added player class

---
+++ src/lav18/unidubna/Player.java
@@ -0,0 +1,33 @@
+package lav18.unidubna;
+
+public class Player {
+    private String name;
+    private int rating;
+    private String sign;
+
+    public Player(String name, String sign) {
+        this.name = name;
+        this.sign = sign;
+        this.rating = 0;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addRating(int rating) {
+        this.rating += rating;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    @Override
+    public String toString() {
+        return "Player " +
+                "'" + name + '\'' +
+                " has rating " + rating +
+                '.';
+    }
+}

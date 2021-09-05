a8bbd7bd570900b387a83d8969514f2d96da56fb
Bnnrymndy@gmail.com <Bnnrymndy@gmail.com>
2021-09-05 10:21:31 +0300

    Init GUI commit

--- src/lav18/unidubna/Controller.java
+++ src/lav18/unidubna/Controller.java
@@ -3,5 +3,37 @@ package lav18.unidubna;
 import java.util.Scanner;
 
 public class Controller {
+    //GUI INIT
+    private Scanner in;
+    private String input;
+    private Game game;
+    private static final Controller instance = new Controller();
 
+    private Controller(){
+        in = new Scanner(System.in);
+        game = Game.getInstance();
+    }
+
+    public static Controller getInstance(){
+        return instance;
+    }
+
+    public void InputStep(){
+        input = in.nextLine();
+        game.Step(Integer.parseInt(input) - 1);
+    };
+
+    public void InputNames(){
+        input = in.nextLine();
+        game.AddPlayers(input.split(" ")[0],input.split(" ")[1]);
+    };
+
+    public int InputMenu(){
+        input = in.nextLine();
+        if(input.trim() == "/q") return 0;
+
+        game.NewGame();
+        return 1;
+
+    };
 }
